2f17571cf3cab96868d3d88ca4714dde9e8a6004
JairGuzman1810 <jair18101@hotmail.com>
2024-09-27 23:44:28 -0600

    Python course step 1 done

---
+++ Python/1. Hello, Python/1. Hello, Python!.py
@@ -0,0 +1,13 @@
+# Python was named for the British comedy troupe Monty Python,
+# so we'll make our first Python program a homage to their skit about Spam.
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
